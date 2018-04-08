77ef0c736e8f4f190015c6d754f55db87490c47b
asitkdash <asitdash@csu.fullerton.edu>
2018-04-08 15:02:25 -0700

    building overlay and content.js updated
    pending images of the buildings.
    divided building.py for better read.
    optimized campusmap.html

--- app/buildingcheck.py
+++ app/buildingcheck.py
@@ -1,23 +1,26 @@
 from .building import getBuilding
+import re
 
 def getBuildingCheck(src):
             classdict=getBuilding()
             abv = ""
-            #src = re.sub("[^a-zA-Z]+", "", src)
-            if len(src) > 3:
-                    if src[-3] in ("0", "1", "2", "3", "4", "5"):
-                            src = src[:-3]
-            # for i in src:
-            #     if i != "-" and i != " ":
-            #         abv = abv + i
-            #     else:
-            #         break
-
+            src = re.sub("[^a-zA-Z]+", "", src)
+            # if len(src) > 3:
+            #         if src[-3] in ("0", "1", "2", "3", "4", "5"):
+            #                 src = src[:-3]
             for i in src:
-                    if i != "-":
-                            abv = abv + i
-                    else:
-                            break
+                if i != "-" and i != " ":
+                    abv = abv + i
+                else:
+                    break
+
+            if len(abv) > 10:
+                abv = abv[:-9]
+            # for i in src:
+            #         if i != "-":
+            #                 abv = abv + i
+            #         else:
+            #                 break
             if abv in classdict:
                     building = classdict[abv]
                     #building.upper()  #makes sure upper AND lower case will work
